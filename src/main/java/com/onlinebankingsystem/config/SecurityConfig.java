5c1abf271638b2b990f75552815d61f58c902db8
victoria-cloud-AE <victoriaenuma@gmail.com>
2026-01-13 05:51:06 +0100

    fixed

--- src/main/java/com/onlinebankingsystem/config/SecurityConfig.java
+++ src/main/java/com/onlinebankingsystem/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.csrf(csrf -> csrf.disable())
-		        .cors(cors -> ())
+		        .cors(cors -> cors.disable())
 		    
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/api/user/login", "/api/user/admin/register").permitAll()
